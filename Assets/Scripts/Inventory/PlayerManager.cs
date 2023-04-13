7d1dbf4d717202a5367243d5f3e120582b7949d6
Zerty123 <a.fm2@orange.fr>
2023-04-13 17:09:06 +0200

    {Add} Get item with keyboard

--- Assets/Scripts/Inventory/PlayerManager.cs
+++ Assets/Scripts/Inventory/PlayerManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
     private Inventory inventory;
     // Start is called before the first frame update
     [SerializeField] private UI_Inventory uiInventory;
+    public GameObject panel_Interaction;
+    private ItemWorld itemWorld2;
     
     void Start()
     {
@@ -15,25 +18,42 @@ public class PlayerManager : MonoBehaviour
 
         ItemWorld.SpawnItemWorld(new Vector3(20, 5), new Item {itemType = Item.ItemType.Flyer, amount = 1});
         ItemWorld.SpawnItemWorld(new Vector3(-20, 5), new Item {itemType = Item.ItemType.CartePostale, amount = 1});
+        panel_Interaction.SetActive(false);        
 
     }
 
 
-    private void OnTriggerEnter (Collider collider)
+    void OnTriggerEnter (Collider collider)
     {
+        panel_Interaction.SetActive(true);
         ItemWorld itemWorld = collider.GetComponent<ItemWorld>();
-        if(itemWorld != null)
-        {
-            //Touching Item
-            inventory.AddItem(itemWorld.GetItem());
-            itemWorld.DestroySelf();
-        }
+        itemWorld2 = itemWorld;
+        // if(itemWorld != null)
+        // {
+        //     inventory.AddItem(itemWorld.GetItem());
+        //     itemWorld.DestroySelf();
+        // }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        panel_Interaction.SetActive(false);
+        itemWorld2 = null;
     }
     
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("space"))
+        {
+            Debug.Log("espace");
+            if(itemWorld2 != null)
+            {
+            inventory.AddItem(itemWorld2.GetItem());
+            itemWorld2.DestroySelf();
+            }
+        }
     }
 }
+
